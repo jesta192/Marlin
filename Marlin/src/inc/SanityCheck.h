78ab6e21215c922260c169c325f435351d5e2e66
jesta192 <jesta192@gmail.com>
2021-04-12 19:57:40 -0400

    disable Z_SAFE_HOMING (and sanity check)

--- Marlin/src/inc/SanityCheck.h
+++ Marlin/src/inc/SanityCheck.h
@@ -1374,9 +1374,10 @@ static_assert(Y_MAX_LENGTH >= Y_BED_SIZE, "Movement bounds (Y_MIN_POS, Y_MAX_POS
     #error "Z_PROBE_LOW_POINT must be less than or equal to 0."
   #endif
 
-  #if HOMING_Z_WITH_PROBE && IS_CARTESIAN && DISABLED(Z_SAFE_HOMING)
-    #error "Z_SAFE_HOMING is recommended when homing with a probe. Enable it or comment out this line to continue."
-  #endif
+  // disabled for Leapfrog XeeD 2015 (probe can reach any point defined for print head on this model)
+  //#if HOMING_Z_WITH_PROBE && IS_CARTESIAN && DISABLED(Z_SAFE_HOMING)
+  //  #error "Z_SAFE_HOMING is recommended when homing with a probe. Enable it or comment out this line to continue."
+  //#endif
 
 #else
 
